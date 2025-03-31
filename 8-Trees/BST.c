f8a18d1a20336320bb4d797339407142460c374e
Sagar <sagarorb@gmail.com>
2025-03-31 11:48:35 +0530

    BST Changes

--- 8-Trees/BST.c
+++ 8-Trees/BST.c
@@ -69,10 +69,91 @@ void postorder(struct TreeNode * T) {
     }
 }
 
+struct TreeNode * findMin(struct TreeNode * T) {
+    if (T == NULL)
+        return NULL;
+
+    while (T->left != NULL)
+        T = T->left;
+
+    return T;
+}
+
+struct TreeNode * delete(struct TreeNode *T, int value) {
+    
+    if (T == NULL) { // Value is not found
+        return NULL;
+    }
+
+    // possibly remove from left subtree
+    if ( value < T->data ) {
+        T->left = delete(T->left , value);
+        return T;
+    }
+
+    // possibly remove from right subtree
+    if (value > T->data) {
+        T->right = delete(T->right, value);
+        return T;
+    }
+
+    // equality case, if condition is optional
+    if (T->data == value) {
+
+        // case 1: deleting the leaf node
+        if (T->left == NULL && T->right == NULL) {
+            free(T); // release the memory allocated for T
+            return NULL;
+        }
+
+        // case 2: deleting the node with one child
+        if (T->left == NULL || T->right == NULL) {
+            struct TreeNode * child;
+            if (T->left == NULL) {
+                child = T->right;
+            } else {
+                child = T->left;
+            }
+            free(T);
+            return child;
+        }    
+
+
+        // case 3: deleting the node with two children
+        // if condition optional
+        if (T->left != NULL && T->right != NULL) {
+             // find the minimum value from right subtree
+             struct TreeNode * minNode = findMin(T->right);   
+             // replace the node value with minimum value from right subtree
+             int minValue = minNode->data;
+             T->data = minValue;
+             T->right = delete(T->right, minValue);
+             return T;
+        }
+    }   
+}
+
+/*
+      10
+   7      12
+*/
+
+int leafCount(struct TreeNode *T) {
+    if (T == NULL)
+       return 0;
+
+    // leaf node
+    if (T->left == NULL && T->right == NULL)
+        return 1;
+
+    return leafCount(T->left) + leafCount(T->right);
+}
+
+
 
 int menu() {
     int choice;
-    printf("\n 1: Insert \n 2: Delete \n 3: Inorder \n 4: Preorder \n 5: Postorder \n 6:Exit \n Enter your choice : ");
+    printf("\n 1: Insert \n 2: Delete \n 3: Inorder \n 4: Preorder \n 5: Postorder \n 6: LeafCount \n 7: Exit \n Enter your choice : ");
     scanf("%d", &choice);
     return choice;
 }
@@ -84,7 +165,7 @@ int main() {
     int value, status;
     struct TreeNode * root = NULL;
 
-    while ( (choice = menu()) != 6) {
+    while ( (choice = menu()) != 7) {
         switch(choice) {
             case 1: 
                     printf(" \n Enter value to insert :");
@@ -92,7 +173,9 @@ int main() {
                     root = insert(root, value);
                     break;
             case 2: 
-                    // delete logic
+                    printf(" \n Enter value to delete :");
+                    scanf("%d", &value);
+                    root = delete(root, value);
                     break;
             case 3:
                     printf("\n inorder values in the tree are \n");
@@ -105,6 +188,10 @@ int main() {
             case 5:
                     printf("\n postorder values in the tree are \n");
                     postorder(root);
+                    break;
+            case 6:
+                    value = leafCount(root);
+                    printf("\n no of leaf nodes are %d \n", value);
                     break;
         }
     }
